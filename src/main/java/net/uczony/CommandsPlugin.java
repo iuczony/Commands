6dae0ec5bedaa8139dd3581bfaa9e6301ed45ccb
iuczony <43303963+iuczony@users.noreply.github.com>
2018-09-16 13:12:28 +0200

    Update CommandsPlugin.java

--- src/main/java/net/uczony/CommandsPlugin.java
+++ src/main/java/net/uczony/CommandsPlugin.java
@@ -2,17 +2,15 @@ package net.uczony;
 
 import net.uczony.listeners.CommandPreProcessListener;
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CommandsPlugin extends JavaPlugin
 {
-    public static CommandsPlugin inst;
+    private static CommandsPlugin inst;
 
     public void onEnable() {
         (CommandsPlugin.inst = this).saveDefaultConfig();
-        Bukkit.getPluginManager().registerEvents((Listener)new CommandPreProcessListener(), (Plugin)this);
+        Bukkit.getPluginManager().registerEvents(new CommandPreProcessListener(), this);
     }
 
     public static CommandsPlugin getInst() {
